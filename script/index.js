d34f022def11b10215f10f96bb633bf474ccc68a
Harshit Sharma <harshits908@gmail.com>
2022-11-09 17:24:09 +0000

    Code restructured

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 //game constants & variables
 let inputDir = {x : 0, y: 0};
-const foodSound = new Audio('food.mp3');
-const gameOverSound = new Audio('gameover.mp3');
-const moveSound = new Audio('move.mp3');
-const musicSound = new Audio('music.mp3');
+const foodSound = new Audio('../res/food.mp3');
+const gameOverSound = new Audio('../res/gameover.mp3');
+const moveSound = new Audio('../res/move.mp3');
+const musicSound = new Audio('../res/music.mp3');
 let speed = 9;
 let score = 0;
 let lastPaintTime = 0;
